45cd9e49e61f1ad9943aff4cb3f221865db88980
Jalifi Maia <jalifimaia@gmail.com>
2025-10-07 13:42:38 -0300

    Pantallas del operario y supervisor

---
+++ Sistema-de-Gestion-de-recursos-y-Seguridad-en-talleres-y-obras/resources/views/detalle-prestamo/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('template_title')
+    Detalle Prestamos
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+
+                            <span id="card_title">
+                                {{ __('Detalle Prestamos') }}
+                            </span>
+
+                             <div class="float-right">
+                                <a href="{{ route('detalle-prestamos.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                  {{ __('Create New') }}
+                                </a>
+                              </div>
+                        </div>
+                    </div>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success m-4">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
+
+                    <div class="card-body bg-white">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+                                        
+									<th >Id Prestamo</th>
+									<th >Id Serie</th>
+									<th >Id Recurso</th>
+
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($detallePrestamos as $detallePrestamo)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            
+										<td >{{ $detallePrestamo->id_prestamo }}</td>
+										<td >{{ $detallePrestamo->id_serie }}</td>
+										<td >{{ $detallePrestamo->id_recurso }}</td>
+
+                                            <td>
+                                                <form action="{{ route('detalle-prestamos.destroy', $detallePrestamo->id) }}" method="POST">
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('detalle-prestamos.show', $detallePrestamo->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('detalle-prestamos.edit', $detallePrestamo->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {!! $detallePrestamos->withQueryString()->links() !!}
+            </div>
+        </div>
+    </div>
+@endsection
