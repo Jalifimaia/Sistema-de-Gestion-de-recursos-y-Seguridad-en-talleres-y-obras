926e2516d97ccab196411969f78b90c7b70bd834
David Cardozo <davidcardozoemma@gmail.com>
2025-10-08 15:04:16 -0300

    Se Agrego las opciones de editar y eliminar a incidentes

--- app/Http/Controllers/IncidenteController.php
+++ app/Http/Controllers/IncidenteController.php
@@ -15,20 +15,37 @@ class IncidenteController extends Controller
         return view('incidente.index', compact('incidentes'));
     }
 
+    public function create()
+    {
+        $recursos = Recurso::all();
+        $estados = EstadoIncidente::all();
+        return view('incidente.create', compact('recursos', 'estados'));
+    }
 
-public function create()
-{
-    $recursos = Recurso::all();
-    $estados = EstadoIncidente::all();
+    public function store(Request $request)
+    {
+        Incidente::create($request->all());
+        return redirect()->route('incidente.index')->with('success', 'Incidente registrado correctamente.');
+    }
 
-    return view('incidente.create', compact('recursos', 'estados'));
-}
+    public function edit($id)
+    {
+        $incidente = Incidente::findOrFail($id);
+        $recursos = Recurso::all();
+        $estados = EstadoIncidente::all();
+        return view('incidente.edit', compact('incidente', 'recursos', 'estados'));
+    }
 
+    public function update(Request $request, $id)
+    {
+        $incidente = Incidente::findOrFail($id);
+        $incidente->update($request->all());
+        return redirect()->route('incidente.index')->with('success', 'Incidente actualizado correctamente.');
+    }
 
-public function store(Request $request)
-{
-    Incidente::create($request->all());
-    return redirect()->route('incidente.index')->with('success', 'Incidente registrado correctamente.');
-}
-
+    public function destroy($id)
+    {
+        Incidente::findOrFail($id)->delete();
+        return redirect()->route('incidente.index')->with('success', 'Incidente eliminado correctamente.');
+    }
 }
