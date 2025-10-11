057ed6abd3c498d11cb369d3fec65b1b9b703b7e
David Cardozo <davidcardozoemma@gmail.com>
2025-10-11 12:48:01 -0300

    borrado : carpeta duplicada

--- Sistema-de-Gestion-de-recursos-y-Seguridad-en-talleres-y-obras/resources/views/partials/barra_navegacion_supervisor.blade.php
+++
@@ -1,8 +0,0 @@
-    <div class="d-flex justify-content-center gap-2 mb-4">
-    <a class="btn btn-outline-primary {{ request()->is('supervisor/checklist-epp') ? 'active' : '' }}" href="{{ url('supervisor/checklist-epp') }}">Checklist EPP</a>
-
-    <a class="btn btn-outline-primary {{ request()->is('supervisor/control-herramientas') ? 'active' : '' }}" href="{{ url('supervisor/control-herramientas') }}">Control Herramientas</a>
-
-    <a class="btn btn-outline-primary {{ request()->is('supervisor/registrar-incidente') ? 'active' : '' }}" href="{{ url('supervisor/registrar-incidente') }}">Registrar Incidente</a>
-
-    </div> 
--- Sistema-de-Gestion-de-recursos-y-Seguridad-en-talleres-y-obras/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\SubcategoriaController;
-
-
-Route::post('/subcategorias', [SubcategoriaController::class, 'store']);
