036c8f40522408655bc21c657968c8c42803e185
Jalifi Maia <jalifimaia@gmail.com>
2025-10-16 17:50:24 -0300

    termine pantalla dashboard

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -5,15 +5,80 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use App\Models\Recurso;
+use Illuminate\Support\Facades\DB;
+
 
 class InventarioController extends Controller
 {
+
     public function index()
     {
         $recursos = Recurso::with(['serieRecursos', 'subcategoria.categoria'])->get();
 
-        return view('inventario', compact('recursos'));
+        $herramientasTotales = DB::table('serie_recurso')
+            ->join('recurso', 'serie_recurso.id_recurso', '=', 'recurso.id')
+            ->join('subcategoria', 'recurso.id_subcategoria', '=', 'subcategoria.id')
+            ->join('categoria', 'subcategoria.categoria_id', '=', 'categoria.id')
+            ->where('categoria.nombre_categoria', 'Herramienta')
+            ->count();
+
+        $herramientasDisponibles = DB::table('serie_recurso')
+            ->join('recurso', 'serie_recurso.id_recurso', '=', 'recurso.id')
+            ->join('subcategoria', 'recurso.id_subcategoria', '=', 'subcategoria.id')
+            ->join('categoria', 'subcategoria.categoria_id', '=', 'categoria.id')
+            ->where('categoria.nombre_categoria', 'Herramienta')
+            ->where('serie_recurso.id_estado', 1)
+            ->count();
+
+        // EPP en stock
+        $eppStock = DB::table('serie_recurso')
+            ->join('recurso', 'serie_recurso.id_recurso', '=', 'recurso.id')
+            ->join('subcategoria', 'recurso.id_subcategoria', '=', 'subcategoria.id')
+            ->join('categoria', 'subcategoria.categoria_id', '=', 'categoria.id')
+            ->where('categoria.nombre_categoria', 'EPP')
+            ->where('serie_recurso.id_estado', 1) // Disponible
+            ->count();
+
+        // EPP totales
+        $eppTotales = DB::table('serie_recurso')
+            ->join('recurso', 'serie_recurso.id_recurso', '=', 'recurso.id')
+            ->join('subcategoria', 'recurso.id_subcategoria', '=', 'subcategoria.id')
+            ->join('categoria', 'subcategoria.categoria_id', '=', 'categoria.id')
+            ->where('categoria.nombre_categoria', 'EPP')
+            ->count();
+
+        // Elementos en reparación
+        $elementosReparacion = DB::table('serie_recurso')
+            ->where('id_estado', 6) // En Reparación
+            ->count();
+
+        $eppVencidos = DB::table('serie_recurso')
+        ->join('recurso', 'serie_recurso.id_recurso', '=', 'recurso.id')
+        ->join('subcategoria', 'recurso.id_subcategoria', '=', 'subcategoria.id')
+        ->join('categoria', 'subcategoria.categoria_id', '=', 'categoria.id')
+        ->where('categoria.nombre_categoria', 'EPP')
+        ->whereNotNull('serie_recurso.fecha_vencimiento')
+        ->where('serie_recurso.fecha_vencimiento', '<', now())
+        ->count();
+
+        $elementosDañados = DB::table('serie_recurso')
+        ->where('id_estado', 5)
+        ->count();
+
+        return view('inventario', compact(
+            'recursos',
+            'herramientasTotales',
+            'herramientasDisponibles',
+            'eppStock',
+            'eppTotales',
+            'elementosReparacion',
+            'eppVencidos',
+            'elementosDañados'
+        ));
     }
+
+
+
     public function exportarCSV()
     {
         $recursos = Recurso::with('subcategoria.categoria', 'serieRecursos')->get();
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -48,9 +48,17 @@ class Usuario extends Model
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function rol()
-    {
-        return $this->belongsTo(\App\Models\Rol::class, 'id_rol', 'id');
+        {
+            return $this->belongsTo(\App\Models\Rol::class, 'id_rol', 'id');
+        }
+        public function trabajador() {
+        return $this->belongsTo(Usuario::class, 'trabajador_id');
     }
+
+    public function supervisor() {
+        return $this->belongsTo(Usuario::class, 'supervisor_id');
+    }
+
     
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
