3e1a3818ef31624fc26a2f50c3b555fbe733da28
mic4barroso <barrosomicaela91@gmail.com>
2025-11-18 17:12:24 -0300

    Instalacion de iconos de bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,12 @@
 
   <!-- Estilos base -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+
 
   <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
 
+  
   <!-- Livewire y estilos inyectados -->
   @livewireStyles
   @stack('styles')
